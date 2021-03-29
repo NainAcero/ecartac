e1d9804e917bbb62178bbca3512fd2c05bf39b1a
NainAceroMamani <a_nacerom@unjbg.edu.pe>
2021-03-28 22:47:02 -0500

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,11 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-
-        if (!\App::environment([
-            'local',
-            'testing',
-        ])) {
+        if($this->app->environment('production')) {
             \URL::forceScheme('https');
         }
     }
